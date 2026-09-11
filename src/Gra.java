28959dc7976ae69197cf48a7484e82ed0195c717
RakUv4 <marek.pilarski@uczen.mechaniktg.pl>
2026-09-11 08:40:56 +0200

    lista trafione

--- src/Gra.java
+++ src/Gra.java
@@ -18,6 +18,33 @@ public class Gra {
     public static HashSet<Integer> getWylosowane() {
         return wylosowane;
     }
+
+    public void wpiszLiczby(){
+        Scanner sc = new Scanner(System.in);
+        wpisane = new ArrayList<>();
+
+        int liczba;
+        for (int i = 0; i < 6; i++){
+            System.out.println("Podaj " + (i + 1) + " liczbę");
+            liczba = sc.nextInt();
+            while (wpisane.contains(liczba)){
+                System.out.println("Taka liczba już była podaj inną");
+                liczba = sc.nextInt();
+            }
+            wpisane.add(liczba);
+        }
+    }
+
+    public LinkedList<Integer> zwrocTrafione(){
+        LinkedList<Integer> trafione = new LinkedList<>();
+        for (Integer wylosowana:wylosowane){
+            if(wpisane.contains(wylosowana)){
+                trafione.add(wylosowana);
+            }
+        }
+
+        return trafione;
+    }
     //mazwa: wylosuj liczby
     //opis: metoda losuje 6 liczb z zakresu 1, 49 bez powtorzen
     // parametry: brak
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
     Gra gra = new Gra();
-        System.out.println(Gra.getWylosowane());
+
+
+        gra.wpiszLiczby();
+        System.out.println("wylosowane: "+ Gra.getWylosowane());
+        System.out.println("wpisane liczby " + gra.getWpisane());
+        System.out.println("trafione " + gra.zwrocTrafione());
 
         Gra gra2 = new Gra();
         System.out.println("te same liczby sie losują "+gra2.getWylosowane());
